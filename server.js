eba9953853480bc3ad10a1d5dbe85e7fd257994b
SabaliDev <127083776+SabaliDev@users.noreply.github.com>
2024-08-21 19:36:52 +0300

    .

--- server.js
+++ server.js
@@ -106,6 +106,6 @@ const startServer = async () => {
 startServer();
 const PORT = 8080;
 
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
